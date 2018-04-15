23a2266b5a63ccccec4426ad946e2b8e788cfab3
javaarchive <raymond.peng07@gmail.com>
2018-04-15 14:57:48 -0700

    Add files via upload

--- Escape_The_Building_(Four_Rooms).py
+++ Escape_The_Building_(Four_Rooms).py
@@ -1,8 +1,19 @@
 import time
 import sys
-
+import platform,os
 Key_Possesion=False
-
+if platform.system()=="Windows":#If running on windows
+  def clear():
+      os.system("cls")
+else:#Other
+    def clear():
+      os.system("clear")
+def running():
+    #RUnning animation
+    for X in range(100):
+        print((X*"*")+"|")
+        time.sleep(0.01)
+        clear()
 def White_Room(key=False):
     White_Direction=input('You are in a white room with doors on three sides of it. To the west is a green door. To the north is a red door. To the east is a brown door. The brown door is locked with a keyhole in it. Choose a direction.')
     if White_Direction=='west':
@@ -36,8 +47,8 @@ def Red_Room():
 
 def Brown_Room(key=False):
     if key:
-        Brown_Direction=input('You are in the brown room. You see an open doorway to the outside world. Type \"exit building\" or a direction to continue')
-        if Brown_Direction=='exit building':
+        Brown_Direction=input('Whew there was no danger! You can go back or continue. To keep going pressWhich Direction now>')
+        if Brown_Direction=='keep walking':
             print('You walk out of the building into the sun.')
             time.sleep(5)
             print('Congratulations! You have exited the building successfully! You successfully took the key to the brown room, entered the brown room, and walked out of the building successfully!')
@@ -47,5 +58,6 @@ def Brown_Room(key=False):
     else:
         print('The brown door is locked')
         White_Room()
-
+running()
 White_Room()
+input("YOu made it!!!!! Go treat yourself to some ice cream!!!!!")
